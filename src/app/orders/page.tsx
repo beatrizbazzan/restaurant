aa0ae458421cad36fc0ac288023db1f6e23c884e
Beatriz Bazzan <69995383+beatrizbazzan@users.noreply.github.com>
2024-02-19 10:48:35 -0300

    Name update.

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -16,23 +16,29 @@ const OrdersPage = () => {
         <tbody>
           <tr className="text-sm md:text-base bg-red-50">
             <td className="hidden md:block py-6 px-1">1237861238721</td>
-            <td className="py-6 px-1">19.07.2023</td>
+            <td className="py-6 px-1">19.02.2024</td>
             <td className="py-6 px-1">89.90</td>
-            <td className="hidden md:block py-6 px-1">Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)</td>
+            <td className="hidden md:block py-6 px-1">
+              Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)
+            </td>
             <td className="py-6 px-1">On the way (approx. 10min)...</td>
           </tr>
           <tr className="text-sm md:text-base odd:bg-gray-100">
             <td className="hidden md:block py-6 px-1">1237861238721</td>
-            <td className="py-6 px-1">19.07.2023</td>
+            <td className="py-6 px-1">19.02.2024</td>
             <td className="py-6 px-1">89.90</td>
-            <td className="hidden md:block py-6 px-1">Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)</td>
+            <td className="hidden md:block py-6 px-1">
+              Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)
+            </td>
             <td className="py-6 px-1">On the way (approx. 10min)...</td>
           </tr>
           <tr className="text-sm md:text-base odd:bg-gray-100">
             <td className="hidden md:block py-6 px-1">1237861238721</td>
-            <td className="py-6 px-1">19.07.2023</td>
+            <td className="py-6 px-1">19.02.2024</td>
             <td className="py-6 px-1">89.90</td>
-            <td className="hidden md:block py-6 px-1">Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)</td>
+            <td className="hidden md:block py-6 px-1">
+              Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)
+            </td>
             <td className="py-6 px-1">On the way (approx. 10min)...</td>
           </tr>
         </tbody>
