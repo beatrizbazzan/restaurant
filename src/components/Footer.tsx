31d7a43033f30324d5730ffdb95a040cf5f9c6f2
Beatriz Bazzan <69995383+beatrizbazzan@users.noreply.github.com>
2024-02-20 23:31:55 -0300

    Deployed in Vercel, README updated with the URL, page title fixed.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
       <Link href="/" className="font-bold text-xl">
-        BEATRIZ BAZZAN
+        POMIDOR
       </Link>
       <p>© ALL RIGHTS RESERVED.</p>
     </div>
